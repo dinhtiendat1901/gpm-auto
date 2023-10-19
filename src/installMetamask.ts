90018a4d4be96949f1180a63d77760cf88c1ff7f
dinhtiendat1901 <dinhtiendat1901@gmail.com>
2023-10-19 12:54:40 +0700

    fix auto click add metamask

--- src/installMetamask.ts
+++ src/installMetamask.ts
@@ -7,6 +7,7 @@ dotenv.config();
 
 export default async function (page: Page) {
     page.setDefaultTimeout(15000);
+    const currentPageCount = (await currentBrowser.pages()).length;
     await page.goto(process.env.WEB_STORE_METAMASK_URL);
     await page.evaluate(() => {
         document.documentElement.requestFullscreen();
@@ -15,7 +16,7 @@ export default async function (page: Page) {
 
     await page.click('[aria-label="Add to Chrome"]');
 
-    while ((await currentBrowser.pages()).length < 2) {
+    while ((await currentBrowser.pages()).length === currentPageCount) {
         await new Promise(r => setTimeout(r, 1000));
         robot.moveMouse(parseInt(process.env.COORDINATES_X), parseInt(process.env.COORDINATES_Y));
         robot.mouseClick();
