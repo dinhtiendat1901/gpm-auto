7788068c08ffafb419916e7f10b16747bbb1c7c5
dinhtiendat1901 <dinhtiendat1901@gmail.com>
2023-10-27 02:11:14 +0700

    update proxy page regSuperfulJob.ts

--- src/job/superful/regSuperfulJob.ts
+++ src/job/superful/regSuperfulJob.ts
@@ -5,6 +5,9 @@ import {writeToSecondSheet} from "../../until/excelUntil";
 export default async function regSuperfulJob(job: Job) {
     try {
         if (job.data.jobTimeout) throw new Error('Job timeout');
+        const proxyPage = await currentBrowser.newPage();
+        proxyPage.setDefaultTimeout(parseInt(process.env.TIME_OUT));
+        await proxyPage.goto(process.env.SUPER_FUL_REG_URL, {waitUntil: 'networkidle0'});
         const regPage = await currentBrowser.newPage();
         regPage.setDefaultTimeout(parseInt(process.env.TIME_OUT));
         await regPage.goto(process.env.SUPER_FUL_REG_URL, {waitUntil: 'networkidle0'});
